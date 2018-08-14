b06350e4813773a98a0c10400def1aa7ebd7d62c
CleoQc <cleoqc1124@gmail.com>
2018-08-14 15:39:24 -0400

    Remove radio control test for now

--- giggletest.ts
+++ giggletest.ts
@@ -51,31 +51,5 @@ let smileNeoPixels = lights.smile()
 smileNeoPixels.showColor(NeoPixelColors.Purple)
 
 
-// Test remote control blocks
-let test_packet = new radio.Packet();
-test_packet.receivedNumber = 50;
-test_packet.receivedString = "50";
-remote.onRemoteControl(1, function (test_packet) {
-remote.remoteControlAction()
-})
-basic.pause(200)
-test_packet.receivedNumber = 0;
-test_packet.receivedString = "0";
-remote.onRemoteControl(1, test_packet)
-        Right = 0
-    }
-
-    // testing the ready made variable for the eyes neopixels
-    let eyesNeoPixels = lights.whichEye(gigglebotWhichEye.Both)
-    eyesNeoPixels.setPixelColor(gigglebotWhichEye.Left, NeoPixelColors.Green)
-    eyesNeoPixels.setPixelColor(gigglebotWhichEye.Right, NeoPixelColors.Yellow)
-    eyesNeoPixels.show()
-
-    // testing the ready made variable for the smile neoPixels
-    let smileNeoPixels = lights.smile()
-    smileNeoPixels.showColor(NeoPixelColors.Purple)
-
-    // Test remote control blocks
-    let test_packet = radio.packet
 
 }
